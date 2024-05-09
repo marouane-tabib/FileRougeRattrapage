fbc7fa53bbeb5761dc3684910535149890a550a0
marouane-tabib <tabibmarouane6@gmail.com>
2024-05-09 20:36:45 +0100

    Initial Commit...

---
+++ src/main/java/com/colome/filerouge/FileRougeApplication.java
@@ -0,0 +1,13 @@
+package com.colome.filerouge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FileRougeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FileRougeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/colome/filerouge/FileRougeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.colome.filerouge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FileRougeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
