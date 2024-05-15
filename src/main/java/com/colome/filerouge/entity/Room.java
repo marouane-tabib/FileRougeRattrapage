a505686f320ea8c868af44cbb3cdee124d56ed59
marouane-tabib <tabibmarouane6@gmail.com>
2024-05-15 10:04:32 +0100

    AddEntities | Make src\main\java\com\colome\filerouge\entity\Room.java file, and Add necessery attributes

---
+++ src/main/java/com/colome/filerouge/entity/Room.java
@@ -0,0 +1,21 @@
+package com.colome.filerouge.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+@Builder
+public class Room {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true, length = 50)
+    private String name;
+}
