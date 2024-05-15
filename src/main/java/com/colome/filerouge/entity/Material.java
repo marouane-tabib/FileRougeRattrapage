dbc37ad12e3db8e5d9a45f535d91d1a374804b3c
marouane-tabib <tabibmarouane6@gmail.com>
2024-05-15 10:04:15 +0100

    AddEntities | Make src\main\java\com\colome\filerouge\entity\Material.java file, and Add necessery attributes

---
+++ src/main/java/com/colome/filerouge/entity/Material.java
@@ -0,0 +1,21 @@
+package com.colome.filerouge.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+@Builder
+public class Material {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true, length = 50)
+    private String name;
+}
