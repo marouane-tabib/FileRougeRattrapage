41d4883776a1c7eaf70fff7b016f1d367971d1ae
marouane-tabib <tabibmarouane6@gmail.com>
2024-05-15 09:32:07 +0100

    AddEntities | Make src\main\java\com\colome\filerouge\entity\Product.java file, and Add Entity attribute and necessery annotation

---
+++ src/main/java/com/colome/filerouge/entity/Product.java
@@ -0,0 +1,24 @@
+package com.colome.filerouge.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+@Builder
+public class Product {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    private String description;
+
+    private Double price;
+}
