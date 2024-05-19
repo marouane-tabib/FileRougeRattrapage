9fbfdf24275bbd530206e809444973cd2bec2f22
marouane-tabib <tabibmarouane6@gmail.com>
2024-05-19 13:25:44 +0100

    PM1-9-AddProductProcess | Make src\main\java\com\colome\filerouge\modules\product\dto\ProductRequestDTO.java file, and Add the validaiton rules and message

---
+++ src/main/java/com/colome/filerouge/modules/product/dto/ProductRequestDTO.java
@@ -0,0 +1,52 @@
+package com.colome.filerouge.modules.product.dto;
+
+import com.colome.filerouge.entity.Product;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.Objects;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class ProductRequestDTO {
+
+    @NotNull(message = "Product name is required")
+    private String name;
+
+    @NotNull(message = "Description is required")
+    private String description;
+
+    @NotNull(message = "Price is required")
+    private Double price;
+
+    @NotNull(message = "Quantity is required")
+    private Integer quantity;
+
+    @NotNull(message = "Sub Category Id is required")
+    private Long subCategoryId;
+
+    @NotNull(message = "Brand Id is required")
+    private Long brandId;
+
+    @NotNull(message = "Unit Id is required")
+    private Long unitId;
+
+    @NotNull(message = "Status is required")
+    private String status;
+
+    // mapping to product
+    public static Product toProduct(ProductRequestDTO productRequestDTO) {
+        return Product.builder()
+                .name(productRequestDTO.getName())
+                .description(productRequestDTO.getDescription())
+                .price(productRequestDTO.getPrice())
+                .status(Objects.equals(productRequestDTO.getStatus(), "Active"))
+                .build();
+    }
+
+}
