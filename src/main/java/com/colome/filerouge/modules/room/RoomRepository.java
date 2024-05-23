faa3c5e12d8128871993f2861a4766bf22062444
marouane-tabib <tabibmarouane6@gmail.com>
2024-05-23 18:27:56 +0100

    PMA-8 | Make src\main\java\com\colome\filerouge\modules\room\RoomRepository.java file, and Add JpaRepository extends

---
+++ src/main/java/com/colome/filerouge/modules/room/RoomRepository.java
@@ -0,0 +1,10 @@
+package com.colome.filerouge.modules.room;
+
+import com.colome.filerouge.entity.Room;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface RoomRepository extends JpaRepository<Room, Long> {
+    Optional<Room> findByName(String name);
+}
