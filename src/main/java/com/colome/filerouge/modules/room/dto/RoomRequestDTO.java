fc1616f507c5cdf5ac0385b1016c870afbb8bb2b
marouane-tabib <tabibmarouane6@gmail.com>
2024-05-24 09:02:22 +0100

    PMA-8 | Change the attributes with the validation rules, and the maping method body - src\main\java\com\colome\filerouge\modules\room\dto\RoomRequestDTO.java

--- src/main/java/com/colome/filerouge/modules/room/dto/RoomRequestDTO.java
+++ src/main/java/com/colome/filerouge/modules/room/dto/RoomRequestDTO.java
@@ -18,24 +18,6 @@ public class RoomRequestDTO {
     @NotNull(message = "Room name is required")
     private String name;
 
-    @NotNull(message = "Description is required")
-    private String description;
-
-    @NotNull(message = "Price is required")
-    private Double price;
-
-    @NotNull(message = "Quantity is required")
-    private Integer quantity;
-
-    @NotNull(message = "Sub Category Id is required")
-    private Long subCategoryId;
-
-    @NotNull(message = "Brand Id is required")
-    private Long brandId;
-
-    @NotNull(message = "Unit Id is required")
-    private Long unitId;
-
     @NotNull(message = "Status is required")
     private String status;
 
@@ -43,8 +25,6 @@ public class RoomRequestDTO {
     public static Room toRoom(RoomRequestDTO roomRequestDTO) {
         return Room.builder()
                 .name(roomRequestDTO.getName())
-                .description(roomRequestDTO.getDescription())
-                .price(roomRequestDTO.getPrice())
                 .status(Objects.equals(roomRequestDTO.getStatus(), "Active"))
                 .build();
     }
