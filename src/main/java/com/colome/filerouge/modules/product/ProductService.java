a13ad27281f2b39ff099d95bb1a65d64622f1232
marouane-tabib <tabibmarouane6@gmail.com>
2024-05-29 09:57:27 +0100

    AddMaterialProcess | Add injection of services to create and update product - src\main\java\com\colome\filerouge\modules\product\ProductService.java

--- src/main/java/com/colome/filerouge/modules/product/ProductService.java
+++ src/main/java/com/colome/filerouge/modules/product/ProductService.java
@@ -1,7 +1,12 @@
 package com.colome.filerouge.modules.product;
 
-import com.colome.filerouge.entity.Product;
+import com.colome.filerouge.entity.*;
 import com.colome.filerouge.handlers.exceptionHandler.ResourceNotFoundException;
+import com.colome.filerouge.modules.category.CategoryServiceInterface;
+import com.colome.filerouge.modules.color.ColorServiceInterface;
+import com.colome.filerouge.modules.material.MaterialServiceInterface;
+import com.colome.filerouge.modules.room.RoomServiceInterface;
+import org.jetbrains.annotations.NotNull;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -9,9 +14,17 @@ import java.util.List;
 @Service
 public class ProductService implements ProductServiceInterface {
     private final ProductRepository productRepository;
+    private final CategoryServiceInterface categoryService;
+    private final RoomServiceInterface roomService;
+    private final MaterialServiceInterface materialService;
+    private final ColorServiceInterface colorService;
 
-    public ProductService(ProductRepository productRepository) {
+    public ProductService(ProductRepository productRepository, CategoryServiceInterface categoryService, RoomServiceInterface roomService, MaterialServiceInterface materialService, ColorServiceInterface colorService) {
         this.productRepository = productRepository;
+        this.categoryService = categoryService;
+        this.roomService = roomService;
+        this.materialService = materialService;
+        this.colorService = colorService;
     }
 
     @Override
@@ -20,6 +33,8 @@ public class ProductService implements ProductServiceInterface {
         if (productRepository.findByName(product.getName()).isPresent()) {
             throw new ResourceNotFoundException("Product with name " + product.getName() + " already exists");
         }
+
+        product = this.setRelations(product);
 
         // save product
         return productRepository.save(product);
@@ -34,8 +49,9 @@ public class ProductService implements ProductServiceInterface {
 
         // get product by id
         Product productToUpdate = this.getProductById(id);
+        
+        product = this.setRelations(product);
 
-        // set sku, slug, category, brand
         productToUpdate.setName(product.getName());
         productToUpdate.setDescription(product.getDescription());
         productToUpdate.setPrice(product.getPrice());
@@ -61,5 +77,18 @@ public class ProductService implements ProductServiceInterface {
     @Override
     public List<Product> getAllProducts() {
         return productRepository.findAll();
+    }
+
+    public Product setRelations(@NotNull Product product) {
+        Category category = categoryService.getCategoryById(product.getCategory().getId());
+        product.setCategory(category);
+        Room room = roomService.getRoomById(product.getCategory().getId());
+        product.setRoom(room);
+        Material material = materialService.getMaterialById(product.getCategory().getId());
+        product.setMaterial(material);
+        Color color = colorService.getColorById(product.getCategory().getId());
+        product.setColor(color);
+
+        return product;
     }
 }
