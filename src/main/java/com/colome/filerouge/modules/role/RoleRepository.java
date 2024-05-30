5d8cba88faea635a0a6e08ea8ffc1a811fd5c11f
marouane-tabib <tabibmarouane6@gmail.com>
2024-05-30 09:48:27 +0100

    AddRoleProcess | Make src\main\java\com\colome\filerouge\modules\role\RoleRepository.java file, and Add extend of JpaRepository

---
+++ src/main/java/com/colome/filerouge/modules/role/RoleRepository.java
@@ -0,0 +1,10 @@
+package com.colome.filerouge.modules.role;
+
+import com.colome.filerouge.entity.Role;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface RoleRepository extends JpaRepository<Role, Long> {
+    Optional<Role> findByName(String name);
+}
