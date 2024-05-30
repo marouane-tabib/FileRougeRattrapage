75737daeee6bf5d8870d4bde8d4e29fd86abca00
marouane-tabib <tabibmarouane6@gmail.com>
2024-05-30 11:04:54 +0100

    AddSpringSecuriterProcess | Make src\main\java\com\colome\filerouge\config\RsaKeyConfigProperties.java file, and Add public record with the publicKey|privateKey attributes

---
+++ src/main/java/com/colome/filerouge/config/RsaKeyConfigProperties.java
@@ -0,0 +1,13 @@
+package com.colome.filerouge.config;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+import java.security.interfaces.RSAPrivateKey;
+import java.security.interfaces.RSAPublicKey;
+
+@ConfigurationProperties(prefix = "rsa")
+public record RsaKeyConfigProperties(
+        RSAPublicKey publicKey,
+        RSAPrivateKey privateKey
+) {
+}
