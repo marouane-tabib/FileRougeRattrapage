dd3f53383f730f008d92a2c247b90131d7d277a3
marouane-tabib <tabibmarouane6@gmail.com>
2024-05-30 10:58:53 +0100

    AddSpringSecuriterProcess | Add EnableConfigurationProperties annotation with RsaKeyConfigProperties.class - src\main\java\com\colome\filerouge\FileRougeApplication.java

--- src/main/java/com/colome/filerouge/FileRougeApplication.java
+++ src/main/java/com/colome/filerouge/FileRougeApplication.java
@@ -1,9 +1,12 @@
 package com.colome.filerouge;
 
+import com.colome.filerouge.config.RsaKeyConfigProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 @SpringBootApplication
+@EnableConfigurationProperties({RsaKeyConfigProperties.class})
 public class FileRougeApplication {
 
 	public static void main(String[] args) {
